e9251d14c23ca63dc046d31ccfda0655bc3b6512
Thorsten Klöhn <thorstenkloehn@gmail.com>
2026-07-30 17:53:32 +0200

    Korrigiere Formatierung der Import-Anweisung in main.rs

--- mein_cli/src/main.rs
+++ mein_cli/src/main.rs
@@ -1,4 +1,4 @@
-use mein_core::{Nachricht,Rolle};
+use mein_core::{Nachricht, Rolle};
 fn main() {
     let nachricht = Nachricht::neu(Rolle::Benutzer,"Hallo wie gehts");
     println!("{:?}",nachricht);
